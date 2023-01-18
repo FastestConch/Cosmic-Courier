e09699dafe893a10d73520c43b3e71323eae0ea1
FastestConch <123scuppers@gmail.com>
2023-01-17 22:03:03 -0500

    Eden Landing Scene Added
    I added a new scene for landing on Eden, but nothing else it just softlocks you atm, im going to add a UI and animation for landing and taking off next

--- Cosmic Courier/Assets/Scripts/Planets/Planet.cs
+++ Cosmic Courier/Assets/Scripts/Planets/Planet.cs
@@ -1,10 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Planet : MonoBehaviour
 {
     public float sizeMultiplier = 2f;
+    public bool inRange = false;
+    public string sceneName;
 
     public void OnTriggerEnter2D(Collider2D other)
     {
@@ -12,13 +15,23 @@ public class Planet : MonoBehaviour
         {
             this.transform.localScale = Vector3.one * sizeMultiplier;
 
+            inRange = true;
+        }
+    }
+
+    public void Update()
+    {
+        if(inRange == true)
+        {
             if(Input.GetKeyDown(KeyCode.E))
             {
                 Debug.Log("PRESSED");
+
+                SceneManager.LoadScene(sceneName, LoadSceneMode.Single);
             }
         }
-
     }
+
     public void OnTriggerExit2D(Collider2D other)
     {
         if(other.CompareTag("Player"))
